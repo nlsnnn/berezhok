649971ab1740921809c66c0c953b53fd00ff5501
nlsnnn <egor.larrr@gmail.com>
2026-03-02 15:34:58 +0300

    init

---
+++ cmd/api/api.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/nlsnnn/berezhok/internal/shared/config"
+)
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID) // important for rate limiting
+	r.Use(middleware.RealIP)    // import for rate limiting and analytics and tracing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer) // recover from crashes
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	return r
+}
+
+func (app *application) run(log *slog.Logger, h http.Handler) error {
+
+	log.Info("starting server", slog.String("address", app.cfg.Address))
+
+	srv := &http.Server{
+		Addr:         app.cfg.Address,
+		Handler:      h,
+		ReadTimeout:  app.cfg.HTTPServer.Timeout,
+		WriteTimeout: app.cfg.Timeout,
+		IdleTimeout:  app.cfg.IdleTimeout,
+	}
+
+	log.Info("server has started", slog.String("address", app.cfg.Address))
+
+	return srv.ListenAndServe()
+}
+
+type application struct {
+	cfg *config.Config
+}
---
+++ cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/nlsnnn/berezhok/internal/shared/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+
+	log.Info("start app", slog.String("env", cfg.Env))
+
+	api := application{
+		cfg: cfg,
+	}
+
+	if err := api.run(log, api.mount()); err != nil {
+		log.Error("failed to start server")
+		os.Exit(1)
+	}
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
